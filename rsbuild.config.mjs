81619ab3c2ba51335c031020a9a2a6a267de89fd
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-26 12:16:05 -0500

    add API key definition in rsbuild.config.mjs for environment configuration

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -2,5 +2,10 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
+  source: {
+    define: {
+      'process.env.API_KEY': JSON.stringify(process.env.RSBUILD_PUBLIC_API_KEY)
+    }
+  },
   plugins: [pluginReact()]
 })
