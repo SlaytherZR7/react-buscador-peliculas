30c64c8cc191292e0c41a59375dd88b206def2f3
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-26 11:08:58 -0500

    add Movies component to display list of movies or no results message

---
+++ src/components/Movies.jsx
@@ -0,0 +1,27 @@
+const ListOfMovies = ({ movies }) => {
+  return (
+    <ul className='movies'>
+      {
+            movies.map(movie => (
+              <li key={movie.id} className='movie'>
+                <h3>{movie.title}</h3>
+                <p>{movie.year}</p>
+                <img src={movie.image} alt={movie.title} />
+              </li>
+            ))
+        }
+    </ul>
+  )
+}
+
+const NoMoviesResults = () => {
+  return <p>No se encontraron películas para esta búsqueda</p>
+}
+
+export const Movies = ({ movies }) => {
+  const hasMovies = movies?.length > 0
+
+  return (
+    hasMovies ? <ListOfMovies movies={movies} /> : <NoMoviesResults />
+  )
+}
