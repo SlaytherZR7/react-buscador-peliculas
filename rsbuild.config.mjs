e1210d182acbf17810a8335f20ff967aa880585b
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-02-26 15:27:19 -0500

    feat: add initial HTML template and SVG icon for movie search application

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from '@rsbuild/core'
 import { pluginReact } from '@rsbuild/plugin-react'
 
 export default defineConfig({
+  html: {
+    template: 'public/index.html'
+  },
   source: {
     define: {
       'process.env.API_KEY': JSON.stringify(process.env.API_KEY)
